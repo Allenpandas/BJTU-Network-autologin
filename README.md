7439ea403cfbe745c2a1dcdcbef39fd3280b34d9
wuyalun <allenpandas@foxmail.com>
2023-01-03 14:47:36 +0800

    提交自动登录脚本

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # BJTU-edu-network-autologin
 北京交通大学校园网自动登录脚本（开机无需输入账号密码，实现自动登录），适用于local.bjtu.edu.cn和web.bjtu.edu.cn
+
+
+
+## 使用教程
+
+克隆仓库代码：
+
+```shell
+git clone https://github.com/wuyalun/BJTU-edu-network-autologin.git
+```
+
+执行python脚本：
+
+```shell
+# 进入到项目目录
+cd BJTU-edu-network-autologin/
+
+# 执行python脚本
+python3 autologin.py
+```
+
---
+++ autologin.py
@@ -0,0 +1,31 @@
+import re
+import requests
+from time import strftime
+import time
+
+# 校园网登录页
+schoolWebURL = 'http://10.10.42.3'  # 10.10.43.3或者10.10.42.3
+
+# 校园网登录的账号（一般是学号或者工号）
+user = ''
+# 校园网登录的密码
+password = ''
+
+while(True):
+    response = requests.get(schoolWebURL)
+
+    # 正则表达式，匹配<title>标签中的内容
+    pattern = re.compile('<title>(.*?)</title>', re.S)
+    title = re.findall(pattern, response.text)
+    title = title[0]  # 将格式转为字符串
+
+    if title == '注销页':
+        print('%s %s 连接正常' % (strftime('%Y-%m-%d'),strftime('%H:%M:%S')))
+        time.sleep(5)
+        pass
+    
+    else:
+        t = str(int(round(time.time() * 1000)))  # 毫秒级时间戳
+        schoolWebLoginURL = schoolWebURL+'/drcom/login?callback=dr'+t+'&DDDDD='+user+'&upass='+password+'&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip=&_='+t
+        print('%s %s 登录成功' % (strftime('%Y-%m-%d'),strftime('%H:%M:%S')))
+        requests.get(schoolWebLoginURL)
